58002532fc66adf03cf0ee88ed29d67a1dfebb96
filipkro <filip.kronstrom@gmail.com>
2021-08-25 19:38:41 +0200

    fix setup to accept tf-cpu

--- setup.py
+++ setup.py
@@ -2,24 +2,36 @@ import setuptools
 from setuptools import setup
 import os
 import sys
+from pkg_resources import DistributionNotFound, get_distribution
+
+def get_dist(pkgname):
+    try:
+        return get_distribution(pkgname)
+    except DistributionNotFound:
+        return None
 
 _here = os.path.abspath(os.path.dirname(__file__))
 
 version = {}
 with open(os.path.join(_here, 'coral_ordinal', 'version.py')) as f:
     exec(f.read(), version)
-    
-    
+
+
 with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+install_deps = ['numpy']
+
+if get_dist('tensorflow') is None and get_dist('tensorflow_gpu') is None and get_dist('tensorflow-cpu') is None:
+    install_deps.append('tensorflow>=2.2')
+
 setup(
     name = 'coral-ordinal',
-    url = 'https://github.com/ck37/coral-ordinal',
-    author = 'Chris Kennedy, Stephen Matthews',
-    author_email = 'chrisken@gmail.com',
+    url = 'https://github.com/filipkro/coral-ordinal',
+    author = 'Chris Kennedy, Stephen Matthews, (Filip Kronström)',
+    author_email = 'filip.kronstrom@gmail.com',
     packages = setuptools.find_packages(),
-    install_requires = ['numpy', 'tensorflow>=2.2'],
+    install_requires = install_deps,
     version = version['__version__'],
     long_description_content_type = "text/markdown",
     license = 'MIT',
